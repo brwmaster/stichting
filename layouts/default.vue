d5724c1670bd421d5ca14749b56cc896226dafc0
Edwin Boon <edwin.boon@eo.nl>
2022-11-09 16:42:36 +0100

    Added pages and layout folder. And add basic header

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="bg-slate-700 h-24">
+    <h2 class="text-white p-8">Stichting met uitgestrekte hand</h2>
+  </div>
+</template>
+
+<style scopes></style>
