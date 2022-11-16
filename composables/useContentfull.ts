77fb3cd738008faab3ba18e20e583a1947cc57ef
Edwin Boon <edwin.boon@eo.nl>
2022-11-16 11:51:21 +0100

    hotfix

--- composables/useContentfull.ts
+++ composables/useContentfull.ts
@@ -1,8 +1,8 @@
 import * as contentful from 'contentful'
 
 const config = {
-    space: 'ey7zpaudq56d',
-    accessToken: 'fIAK8m3xJqHqq5IDUM-NMqLKvQHqr_4yA1WV_V_Wdgk'
+    space: '',
+    accessToken: ''
 }
 
 export const useContentful = async () => await contentful.createClient(config)
