4becc23ccddf085f107948a98141a9eaf360ccca
Edwin Boon <edwin.boon@eo.nl>
2022-11-18 09:52:50 +0100

    Fixed styling for both mobile and desktop. Also added desktop nav

--- components/StHeader.vue
+++ components/StHeader.vue
@@ -8,11 +8,12 @@ const toggleMenu = () => {
 
 <template>
 <header class="fixed h-16 md:h-24 w-full bg-white">
-  <div class="px-4 flex justify-between items-center h-full">
+  <div class="px-4 flex justify-between items-center max-w-screen-xl min-w-screen-lg h-full mx-auto">
       <nuxt-link class="flex" to="/">
           <nuxt-img src="/images/logo.svg" alt="Het logo van Stichting met uitgestrekte hand." class="w-24 md:w-36" placeholder />
       </nuxt-link>
-      <Icon :name="isMenuOpen ? 'mdi:close' : 'mdi:menu-open'" size="3em" class="text-primary md:hidden" @click="toggleMenu"/>
+      <Icon :name="isMenuOpen ? 'mdi:close' : 'mdi:menu-open'" size="3em" class="text-primary sm:hidden" @click="toggleMenu"/>
+      <st-nav class="hidden sm:inline-block"/>
   </div>
   <st-mobile-nav :isMenuOpen="isMenuOpen"/>
 </header>
--- components/StLanding.vue
+++ components/StLanding.vue
@@ -3,10 +3,10 @@
 </script>
 
 <template>
-  <section class="pt-24 px-8 md:pt-32 bg-hero bg-cover h-[80vh] sm:h-[60vh] bg-center md:bg-top flex flex-col items-center justify-center">
+  <section class="pt-24 px-8 md:pt-32 bg-hero bg-cover h-[80vh] sm:h-[60vh] lg:h-[75vh] bg-center md:bg-top flex flex-col items-center justify-center">
     <div class="max-w-screen-md">      
       <h1 class="text-white text-center text-4xl md:text-5xl mb-4 md:mb-6">Stichting met uitgestrekte hand</h1>
-      <p class="text-white text-sm text-center leading-5">Laten we het goede doen, zonder op te geven, want als we niet verzwakken zullen we oogsten wanneer de tijd daarvoor gekomen is. Laten we dus, in de tijd die ons nog rest, voor iedereen het goede doen, vooral voor onze geloofsgenoten. (Galaten 6: 9 en 10)</p>
+      <p class="text-white text-sm md:text-md text-center leading-5">Laten we het goede doen, zonder op te geven, want als we niet verzwakken zullen we oogsten wanneer de tijd daarvoor gekomen is. Laten we dus, in de tijd die ons nog rest, voor iedereen het goede doen, vooral voor onze geloofsgenoten. (Galaten 6: 9 en 10)</p>
     </div>
   </section>
 </template>
